dc865674c36e8acf7669c517ee6bd468097dd14e
Alex Mota <alexmota5@hotmail.com>
2019-07-03 13:18:15 -0300

    change search service-discovery

--- config-server/src/main/docker/run.sh
+++ config-server/src/main/docker/run.sh
@@ -1,7 +1,7 @@
 echo "********************************************************"
 echo "Waiting for the service discovery to start on port 8080"
 echo "********************************************************"
-while ! `nc -z 172.32.0.105 8080 `; do sleep 3; done
+while ! `nc -z service-discovery 8080 `; do sleep 3; done
 echo ">>>>>>>>>>>> Service Discovery has started"
 
 #!/bin/sh
